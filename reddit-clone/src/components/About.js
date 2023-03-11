0164aac5687c1a81f36fc8b403128f797ecba74b
LuisV <luisfvilla012@gmail.com>
2023-03-10 19:10:56 -0800

    design the images and link posts. created a back to top button as well.

--- reddit-clone/src/components/About.js
+++ reddit-clone/src/components/About.js
@@ -4,23 +4,27 @@ const About = () => {
             <div className="about-container">
                 <p className="about-title">About Project</p>
                 <p>
-                    Welcome to What's Poppin'! This was built by me, 
-                    Luis F. Villalon, for my final project of <a href="https://www.theodinproject.com/">The Odin Project</a> JavaScript course. 
-                    This is a simplified clone of the popular website Reddit and was built for learning purposes, 
-                    not to replace said site. If you have any questions or comments, feel free to contact via <a href="https://github.com/LuisFernandoVillalon">Github</a> or <a href="https://www.linkedin.com/in/luis-villalon/">LinkedIn</a>.
+                    Welcome to What's Poppin'! I, Luis F. Villalon, built this site's front-end and set up its back-end services 
+                    using Firebase console, for my final project on <a target="_blank" className="about-link" href="https://www.theodinproject.com/">The Odin Project</a> JavaScript course. 
+                    This is a simplified clone of the popular website, Reddit, made with the intentions of learning. If you have any questions or 
+                    comments, feel free to contact via <a target="_blank" className="about-link" href="https://github.com/LuisFernandoVillalon">Github</a> or <a target="_blank" className="about-link" href="https://www.linkedin.com/in/luis-villalon/">LinkedIn</a>.
                 </p>
             </div>
             <div className="about-container">
                 <p className="about-title">Development Tools</p>
                 <ul>
-                    <li>HTML, CSS, JavaScript</li>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>JavaScript</li>
                     <li>React:
                         <ul>
                             <li>Style-components</li>
                             <li>Router</li>
                             <li>Use-state</li>
+                            <li>Use-Effect</li>
                         </ul>
                     </li>
+                    <li>Bootstrap icons</li>
                     <li>Firebase:
                         <ul>
                             <li>Realtime Database</li>
--- reddit-clone/src/components/HomeBoard.js
+++ reddit-clone/src/components/HomeBoard.js
@@ -1,4 +1,4 @@
-import { BarChartFill, Newspaper, Clock, ArrowUp, ArrowDown, ChatSquare } from 'react-bootstrap-icons'; 
+import { BarChartFill, Newspaper, Clock, ArrowUp, ArrowDown, ChatSquare, BoxArrowUpRight, Link45deg } from 'react-bootstrap-icons'; 
 import { getRecords } from '../MessageBoardSample/firebaseData';
 import { useState, useEffect } from "react";
 
@@ -22,6 +22,10 @@ const HomeBoard = () => {
     )
 }
 
+const backTop = () => {
+    window.scrollTo(0, 0)
+}
+
 const MessageBoard = (props) => {
     const list = props.masterBoard;
     const boardList = list.map((currentPost) => (
@@ -30,27 +34,107 @@ const MessageBoard = (props) => {
         />
 ))
     return (
-        <div>
+        <>
             {boardList}
-        </div>
+            <div className='backtop-button-container'><button className='backtop-button' onClick={() => backTop()}>BACK TO TOP</button></div>
+        </>
     )
 }
 
 const IndividualPost = (props) => {
-    console.log(props.currentPost);
+    const type = props.currentPost.type;
+    if (type === "text") {
+       return ( <TextPost props={props} /> )
+    } else if (type === "image") {
+        return ( <ImagePost props={props} /> )
+    } else if (type === "link") {
+        return ( <LinkPost props={props} /> )
+    }
+}
+
+const directLink = (props) => {
+    window.open(props);
+}
+
+const TextPost = ({props}) => {
     return (
         <div className="individual-post">
-           <div className="first-column">
-                <ArrowUp className='upArrow'/>
-                {props.currentPost.voteAmount}
-                <ArrowDown className='downArrow'/>
-           </div>
-           <div className="second-column">
+        <div className="first-column">
+             <ArrowUp className='upArrow'/>
+             {props.currentPost.voteAmount}
+             <ArrowDown className='downArrow'/>
+        </div>
+        <div className="second-column">
+             <div className='post-user'>Posted by {props.currentPost.user}</div>
+             <div className='post-title'>{props.currentPost.title}</div>
+             <div className='post-comments'><ChatSquare /> {props.currentPost.comments} Comments</div>
+        </div>
+     </div>
+    )
+}
+const ImagePost = ({props}) => {
+    return (
+        <div className="individual-post">
+        <div className="first-column">
+             <ArrowUp className='upArrow'/>
+             {props.currentPost.voteAmount}
+             <ArrowDown className='downArrow'/>
+        </div>
+        <div className="second-column">
+             <div className='post-user'>Posted by {props.currentPost.user}</div>
+             <div className='post-title'>{props.currentPost.title}</div>
+             <img src={props.currentPost.content} className="post-image"/>
+             <div className='post-comments'><ChatSquare /> {props.currentPost.comments} Comments</div>
+        </div>
+     </div>
+    )
+}
+const LinkPost = ({props}) => {
+    let temp = props.currentPost.content;
+     let tempArr = temp.split('');
+    let truncated = [];
+    for (let i = 0; i < tempArr.length; ++i) {
+        if (tempArr[i] === "."  && (tempArr[i+4] === "/" || tempArr[i+3] === "/")) {
+            truncated.push(tempArr[i]);
+            truncated.push(tempArr[i+1]);
+            truncated.push(tempArr[i+2]);
+            truncated.push(tempArr[i+3]);
+            truncated.push(tempArr[i+4]);
+            truncated.push("...")
+            break;
+        }
+        truncated.push(tempArr[i]);
+    }
+    truncated.toString('');
+    
+    return (
+        <div className="individual-post">
+        <div className="first-column">
+             <ArrowUp className='upArrow'/>
+             {props.currentPost.voteAmount}
+             <ArrowDown className='downArrow'/>
+        </div>
+        <div className='post-link-container'>
+            <div className="second-column">
                 <div className='post-user'>Posted by {props.currentPost.user}</div>
                 <div className='post-title'>{props.currentPost.title}</div>
+                <a  target="_blank" className='post-link' href={props.currentPost.content}>{truncated}<BoxArrowUpRight/></a>
                 <div className='post-comments'><ChatSquare /> {props.currentPost.comments} Comments</div>
-           </div>
+            </div>
+            {/* <a  target="_blank" href={props.currentPost.content}> */}
+                <div onClick={() => directLink(props.currentPost.content)} className='link-square'>
+                    <div className='chain-in-square'>
+                        <Link45deg/>
+                    </div>
+                    <div className='link-in-box-container'>
+                        <div className='link-in-box'>
+                            <BoxArrowUpRight/>
+                        </div>
+                    </div>
+                </div>
+            {/* </a> */}
         </div>
+     </div>
     )
 }
 
